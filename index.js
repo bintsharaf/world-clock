c04a553acd7d25a2febdc95ae907b287407d0241
Fatimah Sharafudeen <fatimah.sharafudeen@gmail.com>
2025-12-01 16:13:02 +0100

    Fixed a minor bug

--- index.js
+++ index.js
@@ -46,7 +46,9 @@ function updateCity(event) {
                 </div>
                 
                 <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
-    </div>`
+    </div>
+    <a href="/">All cities</a>
+    `;
 }
 
 
