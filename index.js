ba3f59ad068127a05b828b41145238d4d8cbbdb9
Fatimah Sharafudeen <fatimah.sharafudeen@gmail.com>
2025-12-01 16:02:28 +0100

    Fixed a bug and added an option that updates the user's current location

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-
 function updateTimeZone() {
+    
     //Lagos
     let lagosElement = document.querySelector("#lagos");
     if (lagosElement) {
-        let lagosDate = lagosElement.querySelector(".date");
-        let lagosTimeZone = moment().tz("Africa/Lagos");
-        lagosDate.innerHTML = lagosTimeZone.format("MMMM Do YYYY");
+        let lagosDate = lagosElement.querySelector(".date"); 
         let lagosTime = lagosElement.querySelector(".time");
+        let lagosTimeZone = moment().tz("Africa/Lagos");
+
+        lagosDate.innerHTML = lagosTimeZone.format("MMMM Do YYYY");
         lagosTime.innerHTML =  lagosTimeZone.format("h:mm:ss  [<small>]A[</small>]"); 
     
     }
@@ -15,9 +16,10 @@ function updateTimeZone() {
     let riyadhElement = document.querySelector("#riyadh");
     if (riyadhElement) {
         let riyadhDate = riyadhElement.querySelector(".date");
-        let riyadhTimeZone = moment().tz("Asia/Riyadh");
-        riyadhDate.innerHTML = riyadhTimeZone.format("MMMM Do YYYY");
         let riyadhTime = riyadhElement.querySelector(".time");
+        let riyadhTimeZone = moment().tz("Asia/Riyadh");
+
+        riyadhDate.innerHTML = riyadhTimeZone.format("MMMM Do YYYY");
         riyadhTime.innerHTML =  riyadhTimeZone.format("h:mm:ss  [<small>]A[</small>]");
     }
      
@@ -28,12 +30,16 @@ setInterval(updateTimeZone, 1000);
 
 function updateCity(event) {
     let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+        cityTimeZone = moment.tz.guess();
+    }
+
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
-    citiesElement.innerHTML = 
-    `<div 
-        class="city" id="lagos">
+    citiesElement.innerHTML = `
+    <div 
+        class="city">
                 <div>
                     <h2>${cityName}</h2>
                     <div class="date" id="date">${cityTime.format("MMMM Do YYYY")}</div>
